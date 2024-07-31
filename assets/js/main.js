5e6212fb5aa2f0e890a9e90c6e31da2fc3719476
alisemor21 <morozova.alisa.2001@yandex.ru>
2024-07-31 20:14:22 +0300

    skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,24 @@ navLink.forEach(l => l.addEventListener('click', linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
 
+const skillsContent = document.getElementsByClassName('skills__content');
+const skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className;
+
+    for(let i = 0; i < skillsContent.length; i++){
+        skillsContent[i].classList = 'skills__content skills__close';
+    }
+
+    if (itemClass ==='skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((h)=> {
+    h.addEventListener('click', toggleSkills);
+})
 
 /*==================== QUALIFICATION TABS ====================*/
 
