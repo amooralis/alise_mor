b9c386bafe65aa197fccc20d78abe934f21916f8
alisemor21 <morozova.alisa.2001@yandex.ru>
2024-08-02 16:29:06 +0300

    portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,11 +56,42 @@ skillsHeader.forEach((h)=> {
 
 /*==================== QUALIFICATION TABS ====================*/
 
+// const tabs = document.querySelectorAll('[data-target]');
+// const tabsContents = document.querySelectorAll('[data-content]');
+//
+// tabs.forEach((tab)=> {
+//     tab.addEventListener('click', ()=>{
+//         const target = document.querySelector(tab.dataset.target);
+//
+//         tabsContents.forEach((content)=> content.classList.remove('qualification__active'));
+//         target.classList.add('qualification__active');
+//
+//         tabs.forEach((tab)=> tab.classList.remove('qualification__active'));
+//         tab.classList.add('qualification__active');
+//
+//
+//     })
+// })
+
+
+
 
 /*==================== SERVICES MODAL ====================*/
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+var swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    }
+});
 
 
 /*==================== TESTIMONIAL ====================*/
