676d50063af83e7cf3acbcb49bccdc9ac0fc39ab
alisemor21 <morozova.alisa.2001@yandex.ru>
2024-08-07 16:02:20 +0300

    media small and medium

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.getElementById('nav-close');
@@ -7,7 +6,7 @@ const navClose = document.getElementById('nav-close');
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 
-if(navToggle){
+if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.toggle('show-menu');
     });
@@ -16,7 +15,7 @@ if(navToggle){
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 
-if(navClose){
+if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu');
     });
@@ -25,12 +24,13 @@ if(navClose){
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
-const navLink=document.querySelectorAll('.nav__link');
+const navLink = document.querySelectorAll('.nav__link');
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu');
     navMenu.classList.remove('show-menu');
 }
+
 navLink.forEach(l => l.addEventListener('click', linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
@@ -38,19 +38,19 @@ navLink.forEach(l => l.addEventListener('click', linkAction));
 const skillsContent = document.getElementsByClassName('skills__content');
 const skillsHeader = document.querySelectorAll('.skills__header');
 
-function toggleSkills(){
+function toggleSkills() {
     let itemClass = this.parentNode.className;
 
-    for(let i = 0; i < skillsContent.length; i++){
+    for (let i = 0; i < skillsContent.length; i++) {
         skillsContent[i].classList = 'skills__content skills__close';
     }
 
-    if (itemClass ==='skills__content skills__close'){
+    if (itemClass === 'skills__content skills__close') {
         this.parentNode.className = 'skills__content skills__open'
     }
 }
 
-skillsHeader.forEach((h)=> {
+skillsHeader.forEach((h) => {
     h.addEventListener('click', toggleSkills);
 })
 
@@ -72,8 +72,6 @@ skillsHeader.forEach((h)=> {
 //
 //     })
 // })
-
-
 
 
 /*==================== SERVICES MODAL ====================*/
@@ -99,18 +97,86 @@ var swiper = new Swiper(".portfolio__container", {
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY < (sectionTop + sectionHeight)) {
+            document.querySelector('.nav__menu a[href*="' + sectionId + '"]').classList.add('active--link');
+        } else {
+            document.querySelector('.nav__menu a[href*="' + sectionId + '"]').classList.remove('active--link');
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
+
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    if (this.scrollY >= 80) {
+        nav.classList.add('scroll--header')
+    } else {
+        nav.classList.remove('scroll--header')
+    }
+}
+
+window.addEventListener('scroll', scrollHeader);
 
 /*==================== SHOW SCROLL UP ====================*/
 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if (this.scrollY > 500) {
+        scrollUp.classList.add('show-scroll');
+    } else {
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+
+window.addEventListener('scroll', scrollUp);
 
 /*==================== DARK LIGHT THEME ====================*/
 
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
 
 
+if (selectedTheme){
 
+    document.body.classList[selectedTheme === 'dark' ? 'add': 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add': 'remove'](iconTheme);
+}
+
+
+document.body.classList.toggle(darkTheme);
+themeButton.classList.add(iconTheme);
+
+
+themeButton.addEventListener('click',()=>{
+
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+
+})
 
 // window.addEventListener('blur',()=>{
 //     document.title = 'Вернитесь 🥲'
